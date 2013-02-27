a47623e381feb3d1bd42f4674d4f75c1d701628b
Jason Huntington <jasonmhuntington@gmail.com>
2013-02-26 18:36:51 -0600

    working radio buttons

--- src/documents/assets/scripts/app.js
+++ src/documents/assets/scripts/app.js
@@ -2,8 +2,33 @@
 
 var app = angular.module('esFitApp', ['$strap.directives']);
 
-app.controller('StrapCtrl', function($scope) {
-  $scope.radio = {tressite: true, sietesite: false};
-  $scope.radioValue = 'tressite';
+app.controller('BodyCalcCtrl', function($scope) {
+  $scope.button = {active: true};
+  $scope.buttonSelect = {price: '89,99', currency: '€'};
+  $scope.checkbox = {left: false, middle: true, right: false};
+  $scope.radio = {left: false, middle: true, right: false};
+  $scope.radioValue = 'middle';
+
+  $scope.datepicker = {date: ''};
+  $scope.timepicker = {time: ''};
+
+
+  $scope.sitenumber = 'threesite';
+  $scope.clientgender = 'male';
+
+  $scope.master = {};
+
+  $scope.clientstats= {};
+
+  $scope.calculate = function(client) {
+    $scope.clientstats= angular.copy(client);
+  };
+
+  $scope.reset = function() {
+    $scope.client = angular.copy($scope.master);
+  };
+
+  $scope.reset();
+
 
 });
